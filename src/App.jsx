4bdd1d299d6e25f5892b354b5ca81d00eb20c349
Wyvern009 <mfadhli2803@gmail.com>
2026-06-24 15:50:23 +0700

    Setup Layout and AppContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import { Sidebar } from "./components/Sidebar"
+import { ChatBox } from "./components/ChatBox"
+import { Credits } from "./pages/Credits"
+import { Community } from "./pages/Community"
+
 function App() {
 
   return (
-    <div>
-      <h1 className="text-5xl text-blue-700">Hello</h1>
-    </div>
+    <>
+      <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar></Sidebar>
+          <Routes>
+            <Route path="/" element={<ChatBox />}></Route>
+            <Route path="/credits" element={<Credits />}></Route>
+            <Route path="/community" element={<Community />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </>
   )
 }
 
