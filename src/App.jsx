e81ceca077d6d4f2a8799cfe55e3d1c016583dab
Wyvern009 <mfadhli2803@gmail.com>
2026-06-30 16:59:55 +0700

    Membangun halaman loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import { Sidebar } from "./components/Sidebar"
 import { ChatBox } from "./components/ChatBox"
 import { Credits } from "./pages/Credits"
@@ -6,9 +6,13 @@ import { Community } from "./pages/Community"
 import { useState } from "react"
 import { assets } from "./assets/assets"
 import "./assets/prism.css";
+import { Loading } from "./pages/Loading"
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const {pathname} = useLocation(); 
+
+  if (pathname === "/loading") return <Loading />
 
   return (
     <>
