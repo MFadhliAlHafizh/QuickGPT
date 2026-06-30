e6c092af58a9427bee194ee2d5c1af5a12b8c394
Wyvern009 <mfadhli2803@gmail.com>
2026-06-30 16:04:41 +0700

    Membangun halaman chatBox dan menampilkan dummy messages

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,5 +1,113 @@
+import { useEffect, useRef, useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
+import { Message } from "./Message";
+
 export const ChatBox = () => {
+  const { selectedChat, theme } = useAppContext();
+
+  const containerRef = useRef(null);
+
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [prompt, setPrompt] = useState("");
+  const [mode, setMode] = useState("text");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages);
+    }
+  }, [selectedChat]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
   return (
-    <div>ChatBox</div>
-  )
-}
+    <div className="flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14">
+      {/* Chat Messages */}
+      <div ref={containerRef} className="flex-1 mb-5 overflow-y-scroll">
+        {messages.length === 0 && (
+          <div className="h-full flex flex-col items-center justify-center gap-2 text-primary">
+            <img
+              src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+              alt="Logo"
+              className="w-full max-w-56 sm:max-w-68"
+            />
+
+            <p className="mt-5 text-4xl sm:text-6xl text-center text-gray-400 dark:text-white">
+              Ask me anything.
+            </p>
+          </div>
+        )}
+
+        {messages.map((message, index) => (
+          <Message key={index} message={message} />
+        ))}
+
+        {/* Three Dots Loading */}
+        {loading && (
+          <div className="loader flex items-center gap-1.5">
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+          </div>
+        )}
+      </div>
+
+      {mode === "image" && (
+        <label className="inline-flex items-center gap-2 text-sm mb-3 mx-auto">
+          <p className="text-xs">Publish Generated Image to Community</p>
+          <input
+            type="checkbox"
+            checked={isPublished}
+            onChange={(e) => setIsPublished(e.target.value)}
+            className="cursor-pointer"
+          />
+        </label>
+      )}
+
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full py-3 px-4 bg-primary/20 dark:bg-[#583C79] border border-primary dark:border-[#80609F]/30 rounded-full flex items-center gap-4"
+      >
+        <select
+          onChange={(e) => setMode(e.target.value)}
+          value={mode}
+          className="text-sm pl-3 outline-none"
+        >
+          <option value="text" className="dark:bg-purple-900">
+            Text
+          </option>
+          <option value="image" className="dark:bg-purple-900">
+            Image
+          </option>
+        </select>
+        <input
+          type="text"
+          placeholder="Type your prompt here..."
+          onChange={(e) => setPrompt(e.target.value)}
+          value={prompt}
+          className="flex-1 w-full text-sm outline-none"
+        />
+        <button type="submit" disabled={loading}>
+          <img
+            src={loading ? assets.stop_icon : assets.send_icon}
+            alt="Send Button"
+            className="w-8 cursor-pointer"
+          />
+        </button>
+      </form>
+    </div>
+  );
+};
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,46 @@
-export const Message = () => {
+import { assets } from "../assets/assets";
+import moment from "moment";
+import Markdown from "react-markdown";
+import Prism from "prismjs";
+import { useEffect } from "react";
+
+export const Message = ({ message }) => {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [message.content]);
+
   return (
-    <div>Message</div>
-  )
-}
+    <div>
+      {message.role === "user" ? (
+        <div className="flex items-start justify-end gap-2 my-4">
+          <div className="flex flex-col gap-2 py-2 px-4 bg-slate-50 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl">
+            <p className="text-sm dark:text-primary">{message.content}</p>
+            <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">
+              {moment(message.timestamp).fromNow()}
+            </span>
+          </div>
+          <img
+            src={assets.user_icon}
+            alt="User Icon"
+            className="w-8 rounded-full"
+          />
+        </div>
+      ) : (
+        <div className="flex gap-2 my-4">
+          <div className="flex flex-col gap-2 py-2 px-4 bg-slate-50 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl mt-4">
+            {message.isImage ? (
+              <img
+                src={message.content}
+                alt="Content Image"
+                className="w-full max-w-md mt-2 rounded-md"
+              />
+            ) : (
+              <p className="text-sm dark:text-primary reset-tw"><Markdown>{message.content}</Markdown></p>
+            )}
+            <span className="text-xs text-gray-400 dark:text-[#B1A6C0]">{moment(message.timestamp).fromNow()}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
